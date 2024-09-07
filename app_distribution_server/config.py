753defb4d46fdc33d6da7908cdeb9d7d66001526
Francisco Marques <franciscopcmarques@gmail.com>
2024-09-07 12:28:22 +0100

    Improve disabling the URL and improve documentation

--- app_distribution_server/config.py
+++ app_distribution_server/config.py
@@ -13,8 +13,9 @@ if not UPLOADS_SECRET_AUTH_TOKEN:
         " For security reasons override it with the 'UPLOADS_SECRET_AUTH_TOKEN' env var.",
     )
 
-
 APP_BASE_URL = os.getenv("APP_BASE_URL", "http://localhost:8000")
 APP_VERSION = os.getenv("APP_VERSION") or "0.0.1-development"
 APP_TITLE = os.getenv("APP_TITLE") or "iOS/Android app distribution server"
-LOGO_URL = os.getenv("LOGO_URL", "/static/logo.svg")
+
+_raw_logo_url = os.getenv("LOGO_URL", "/static/logo.svg")
+LOGO_URL: str | None = None if _raw_logo_url.lower() in ["", "0", "false"] else _raw_logo_url
